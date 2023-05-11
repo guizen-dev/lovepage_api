81ac258ef10a7048e049982b3f3816f16af34338
Joelson Campos <guilherme.campos3931@gmail.com>
2023-05-11 18:20:59 -0300

    urls update 2

--- lovepageapi/urls.py
+++ lovepageapi/urls.py
@@ -6,8 +6,8 @@ import os
 
 urlpatterns = [
     path(str(os.getenv('API_KEY'))+'/admin/', admin.site.urls),
-    path(str(os.getenv('API_KEY'))+'api-auth/', include('rest_framework.urls')),
-    path(str(os.getenv('API_KEY'))+'usuarios/', include('usuario.urls')),
-    path(str(os.getenv('API_KEY'))+'features/', include('features.urls')),
-    path(str(os.getenv('API_KEY'))+'notes/', include('chat.urls')),
+    path(str(os.getenv('API_KEY'))+'/api-auth/', include('rest_framework.urls')),
+    path(str(os.getenv('API_KEY'))+'/usuarios/', include('usuario.urls')),
+    path(str(os.getenv('API_KEY'))+'/features/', include('features.urls')),
+    path(str(os.getenv('API_KEY'))+'/notes/', include('chat.urls')),
 ]
