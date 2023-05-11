1ec89fd06f8c1ad2dd29bed21c6e5d65b99abc6f
Joelson Campos <guilherme.campos3931@gmail.com>
2023-05-11 15:48:42 -0300

    allow all

--- lovepageapi/settings.py
+++ lovepageapi/settings.py
@@ -137,8 +137,8 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ORIGIN_ALLOW_ALL = False
-CORS_ORIGIN_WHITELIST = (
-    'http://localhost:8081',
-    'love-pageapi.onrender.com'
-)
+CORS_ORIGIN_ALLOW_ALL = True
+#CORS_ORIGIN_WHITELIST = (
+#    'http://localhost:8081',
+#    'love-pageapi.onrender.com'
+#)
